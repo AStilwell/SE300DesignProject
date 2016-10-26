f807c3636f9dd9a4d8190c09e8b04129f5533297
Andrew Stilwell <Andrew Stilwell@DESKTOP-3N950UJ>
2016-10-26 16:26:04 -0400

    Initial Commit

---
+++ SE300DesignProject/Diagram.java
@@ -0,0 +1,247 @@
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by Andrew Stilwell on 24-Oct-16.
+ */
+public class Diagram {
+
+    private static final String filePath = ".\\src\\GUI\\Test.xls";
+    ArrayList<VBox> storage = new ArrayList<>();
+    ArrayList<VBox> nodeList = new ArrayList<>();
+
+    /**
+     * @return Generates an array list of appropriate nodes for use in the main GUI creation.
+     * @throws BiffException
+     * @throws IOException
+     */
+    public ArrayList<VBox> read() throws BiffException, IOException {
+
+        VBox command = new VBox();
+
+        VBox wepsAll = new VBox();
+        VBox wepsCommand = new VBox();
+        VBox caStaff = new VBox();
+        VBox cgStaff = new VBox();
+        VBox caDiv = new VBox();
+        VBox cgDiv = new VBox();
+        VBox ca01 = new VBox();
+        VBox ca02 = new VBox();
+        VBox cg01 = new VBox();
+        VBox cg02 = new VBox();
+
+        HBox caPersonnel = new HBox();
+        HBox cgPersonnel = new HBox();
+        HBox wepsMain = new HBox();
+
+        VBox wepsDiagram = new VBox();
+
+        VBox engAll = new VBox();
+        VBox engCommand = new VBox();
+        VBox emStaff = new VBox();
+        VBox eaStaff = new VBox();
+        VBox emDiv = new VBox();
+        VBox eaDiv = new VBox();
+        VBox em01 = new VBox();
+        VBox em02 = new VBox();
+        VBox ea01 = new VBox();
+        VBox ea02 = new VBox();
+
+        HBox emPersonnel = new HBox();
+        HBox eaPersonnel = new HBox();
+        HBox engMain = new HBox();
+
+        VBox engDiagram = new VBox();
+
+
+        File file = new File(filePath);
+        Workbook workbook = Workbook.getWorkbook(file);
+        Sheet sheet = workbook.getSheet(0);
+
+        for (int j = 0; j < sheet.getRows(); j++) {
+            Cell cell = sheet.getCell(0, j);
+            Cell workCenter = sheet.getCell(1, j);
+            switch (cell.getContents()) {
+                case "BN STAFF":
+                    command.getChildren().add(createPane(sheet.getCell(2, j).getContents(), sheet.getCell(3, j).getContents(), sheet.getCell(4, j).getContents()));
+                case "WEPS":
+                    switch (workCenter.getContents()) {
+                        case "STAFF":
+                            wepsCommand.getChildren().add(createPane(sheet.getCell(2, j).getContents(), sheet.getCell(3, j).getContents(), sheet.getCell(4, j).getContents()));
+                            break;
+                        case "CA":
+                            caStaff.getChildren().add(createPane(sheet.getCell(2, j).getContents(), sheet.getCell(3, j).getContents(), sheet.getCell(4, j).getContents()));
+                            break;
+                        case "CA-01":
+                            ca01.getChildren().add(createPane(sheet.getCell(2, j).getContents(), sheet.getCell(3, j).getContents(), sheet.getCell(4, j).getContents()));
+                            break;
+                        case "CA-02":
+                            ca02.getChildren().add(createPane(sheet.getCell(2, j).getContents(), sheet.getCell(3, j).getContents(), sheet.getCell(4, j).getContents()));
+                            break;
+                        case "CG":
+                            cgStaff.getChildren().add(createPane(sheet.getCell(2, j).getContents(), sheet.getCell(3, j).getContents(), sheet.getCell(4, j).getContents()));
+                            break;
+                        case "CG-01":
+                            cg01.getChildren().add(createPane(sheet.getCell(2, j).getContents(), sheet.getCell(3, j).getContents(), sheet.getCell(4, j).getContents()));
+                            break;
+                        case "CG-02":
+                            cg02.getChildren().add(createPane(sheet.getCell(2, j).getContents(), sheet.getCell(3, j).getContents(), sheet.getCell(4, j).getContents()));
+                            break;
+                    }
+                    break;
+                case "ENG":
+                    switch (workCenter.getContents()) {
+                        case "STAFF":
+                            engCommand.getChildren().add(createPane(sheet.getCell(2, j).getContents(), sheet.getCell(3, j).getContents(), sheet.getCell(4, j).getContents()));
+                            break;
+                        case "EA":
+                            eaStaff.getChildren().add(createPane(sheet.getCell(2, j).getContents(), sheet.getCell(3, j).getContents(), sheet.getCell(4, j).getContents()));
+                            break;
+                        case "EA-01":
+                            ea01.getChildren().add(createPane(sheet.getCell(2, j).getContents(), sheet.getCell(3, j).getContents(), sheet.getCell(4, j).getContents()));
+                            break;
+                        case "EA-02":
+                            ea02.getChildren().add(createPane(sheet.getCell(2, j).getContents(), sheet.getCell(3, j).getContents(), sheet.getCell(4, j).getContents()));
+                            break;
+                        case "EM":
+                            emStaff.getChildren().add(createPane(sheet.getCell(2, j).getContents(), sheet.getCell(3, j).getContents(), sheet.getCell(4, j).getContents()));
+                            break;
+                        case "EM-01":
+                            em01.getChildren().add(createPane(sheet.getCell(2, j).getContents(), sheet.getCell(3, j).getContents(), sheet.getCell(4, j).getContents()));
+                            break;
+                        case "EM-02":
+                            em02.getChildren().add(createPane(sheet.getCell(2, j).getContents(), sheet.getCell(3, j).getContents(), sheet.getCell(4, j).getContents()));
+                            break;
+                    }
+                    break;
+                case "NAV":
+                    //TODO
+                    break;
+                case "MO":
+                    //TODO
+                    break;
+            }
+        }
+
+        caPersonnel.getChildren().addAll(ca01, ca02);
+        caPersonnel.setSpacing(15);
+        cgPersonnel.getChildren().addAll(cg01, cg02);
+        cgPersonnel.setSpacing(15);
+        caDiv.getChildren().addAll(caStaff, caPersonnel);
+        caDiv.setSpacing(15);
+        cgDiv.getChildren().addAll(cgStaff, cgPersonnel);
+        cgDiv.setSpacing(15);
+        wepsMain.getChildren().addAll(caDiv, cgDiv);
+        wepsMain.setSpacing(15);
+
+        wepsMain.setAlignment(Pos.CENTER);
+
+        wepsAll.getChildren().addAll(wepsCommand, wepsMain);
+        wepsAll.setSpacing(15);
+
+        wepsDiagram.getChildren().addAll(wepsAll);
+
+        eaPersonnel.getChildren().addAll(ea01, ea02);
+        eaPersonnel.setSpacing(15);
+        emPersonnel.getChildren().addAll(em01, em02);
+        emPersonnel.setSpacing(15);
+        eaDiv.getChildren().addAll(eaStaff, eaPersonnel);
+        eaDiv.setSpacing(15);
+        emDiv.getChildren().addAll(emStaff, emPersonnel);
+        emDiv.setSpacing(15);
+        engMain.getChildren().addAll(eaDiv, emDiv);
+        engMain.setSpacing(15);
+        engAll.getChildren().addAll(engCommand, engMain);
+        engAll.setSpacing(15);
+
+        engMain.setAlignment(Pos.CENTER);
+
+        engDiagram.getChildren().addAll(engAll);
+
+        storage.add(0, command);
+        storage.add(1, wepsDiagram);
+        storage.add(2, engDiagram);
+
+        return storage;
+    }
+
+    /**
+     * @param p The position of the row being read.
+     * @param n The name of the row being read.
+     * @param e The email of the row being read.
+     * @return The created graphic node that contains name, position, and email
+     */
+    private VBox createPane(String p, String n, String e) {
+
+        VBox node = new VBox();
+
+        Text title = new Text();
+        title.setText("TITLE: " + p);
+
+        Text name = new Text();
+        name.setText("NAME: " + n);
+
+        Text email = new Text();
+        email.setText("EMAIL: " + e);
+
+        node.getChildren().addAll(title, name, email);
+
+        nodeList.add(node);
+
+        System.out.println("Node Created: " + p);
+
+        node.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            EmailList temp = new EmailList();
+
+            @Override
+            public void handle(MouseEvent event) {
+                MouseButton button = event.getButton();
+                if (button == MouseButton.PRIMARY) {
+                    node.setStyle("-fx-background-color: yellow; -fx-border-color: black");
+                    temp.addEmail(temp.getEmailList(), e);
+                    temp.addName(temp.getNameList(), n);
+                } else if (button == MouseButton.SECONDARY) {
+                    node.setStyle("-fx-background-color: transparent; -fx-border-color: black");
+                    temp.delEmail(temp.getEmailList(), e);
+                    temp.delName(temp.getEmailList(), n);
+                }
+            }
+        });
+
+        node.setStyle("-fx-border-color: black");
+        node.setSpacing(5);
+        return node;
+    }
+
+    /**
+     * @return The final diagram created by this class
+     */
+    public ArrayList<VBox> getDiagram() {
+        return storage;
+    }
+
+    public void clearSelection() {
+        for (int i = 0; i < nodeList.size(); i++) {
+            nodeList.get(i).setStyle("-fx-background-color: transparent; -fx-border-color: black");
+        }
+    }
+
+    public void selectAll(){
+        for (int i = 0; i < nodeList.size(); i++) {
+            nodeList.get(i).setStyle("-fx-background-color: yellow; -fx-border-color: black");
+        }
+    }
+}
---
+++ SE300DesignProject/UserInterface.java
@@ -0,0 +1,128 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import jxl.read.biff.BiffException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by stilw on 17-Oct-16.
+ */
+public class UserInterface extends Application {
+
+    /**
+     * @param primaryStage Stage default for creation of GUI.
+     */
+    @Override
+    public void start(Stage primaryStage) {
+        //Create new email list
+        EmailList email = new EmailList();
+
+        //Create new diagram handling class
+        Diagram diagram = new Diagram();
+        try {
+            diagram.read();
+        } catch (IOException | BiffException ex) {
+            ex.printStackTrace();
+        }
+
+        ArrayList<VBox> chart = diagram.getDiagram();
+
+        //Create various root panes for main GUI window
+        BorderPane diagramHandler = new BorderPane();
+        BorderPane root = new BorderPane();
+
+        //Create sub-panes that contain controls and text fields
+        GridPane controls = new GridPane();
+        TextArea emails = new TextArea();
+        emails.setPromptText("No Emails Selected");
+        emails.setWrapText(true);
+        TextArea message = new TextArea();
+        message.setPromptText("Enter Message Here");
+        Button massRecall = new Button("RECALL");
+
+        //Method to select all email addresses and add them to the email list
+        massRecall.setOnMouseClicked(fn -> {
+            try {
+                email.getAllEmails();
+            } catch (BiffException | IOException e) {
+                System.out.println("There was an error getting the correct email addresses.");
+            }
+            diagram.selectAll();
+            emails.setText(email.outputEmailList());
+            message.setText("This is a RECALL, all-hands must respond to this email by clicking the given link.");
+        });
+
+        //Button declarations and functions
+        Button select = new Button("SELECT");
+        select.setOnMouseClicked(fn -> {
+            emails.setText(email.outputEmailList());
+            System.out.println(email.getEmailList());
+            System.out.println(email.getNameList());
+        });
+
+        Button clear = new Button("CLEAR");
+        clear.setOnAction(fn -> {
+            diagram.clearSelection();
+            email.getEmailList().clear();
+            emails.setText(null);
+            message.setText(null);
+        });
+
+        //Add buttons to the control sub-pane
+        controls.add(emails, 0, 0, 2, 1);
+        controls.add(message, 0, 1, 2, 1);
+        controls.add(massRecall, 0, 2);
+        controls.add(select, 1, 2);
+        controls.add(clear, 0, 3);
+        controls.setMinWidth(250);
+        controls.setHgap(20);
+        controls.setVgap(20);
+
+        //Create new menu
+        MenuBar menuBar = new MenuBar();
+
+        //Various menu item creation
+        Menu file = new Menu("File");
+        MenuItem exit = new MenuItem("Exit");
+        exit.setOnAction(e -> Platform.exit());
+        file.getItems().addAll(exit);
+
+        Menu help = new Menu("Help");
+
+        Menu department = new Menu("Switch View");
+        MenuItem command = new MenuItem("BN STAFF");
+        command.setOnAction(event -> diagramHandler.setCenter(chart.get(0)));
+        MenuItem weapons = new MenuItem("WEAPONS");
+        weapons.setOnAction(event -> diagramHandler.setCenter(chart.get(1)));
+        MenuItem engineering = new MenuItem("ENGINEERING");
+        engineering.setOnAction(event -> diagramHandler.setCenter(chart.get(2)));
+        MenuItem navigation = new MenuItem("NAVIGATION");
+        department.getItems().addAll(command, weapons, engineering, navigation);
+
+        menuBar.getMenus().addAll(file, help, department);
+
+        //Add default diagram to diagram handler
+        diagramHandler.setCenter(chart.get(0));
+
+        //Add elements to main root pane
+        root.setTop(menuBar);
+        root.setCenter(diagramHandler);
+        root.setLeft(controls);
+
+        //Create new scene
+        Scene startScreen = new Scene(root);
+
+        //Create new stage and assign scene/attributes
+        primaryStage.setScene(startScreen);
+        primaryStage.setTitle("User Interface");
+        primaryStage.setMaximized(true);
+        primaryStage.show();
+    }
+}
