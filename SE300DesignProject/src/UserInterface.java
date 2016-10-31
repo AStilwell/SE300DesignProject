3836346cd8eb607e98da3514bb8fce0b9247a74e
Andrew Stilwell <Andrew Stilwell@DESKTOP-3N950UJ>
2016-10-31 14:07:33 -0400

    Updated password field/troubleshooting

--- SE300DesignProject/src/UserInterface.java
+++ SE300DesignProject/src/UserInterface.java
@@ -85,10 +85,10 @@ public class UserInterface extends Application
         
         Button send = new Button("SEND");
         send.setOnMouseClicked(fn -> {
-        	TextField password = new TextField();
+        	PasswordField password = new PasswordField();
         	Button ok = new Button("OK");
         	ok.setOnMouseClicked(sending -> {
-        		MailSender.sendEmail("stilwell.andrewk@gmail.com", password.getCharacters().toString(), email.outputEmailList(), "Test", "This is a test");
+        		sendEmail("stilwell.andrewk@gmail.com", password.getCharacters().toString(), email.outputEmailList(), "Test", "This is a test");
         	});
         	
         	Button cancel = new Button("Cancel");
