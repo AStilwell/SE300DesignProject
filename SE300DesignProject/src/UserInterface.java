47f9ef95c1fa2b3a9db8f237abb0832bda4d090c
Andrew Stilwell <Andrew Stilwell@DESKTOP-3N950UJ>
2016-10-31 15:55:35 -0400

    Optimized imports

--- SE300DesignProject/src/UserInterface.java
+++ SE300DesignProject/src/UserInterface.java
@@ -4,7 +4,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import jxl.read.biff.BiffException;
